dc2a9b81fa469fd6a71314b26c94956d777ee5e9
leafney <babycoolzx@126.com>
2024-01-13 00:28:50 +0800

    feat: start

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/leafney/music-grabber
+
+go 1.21.4
+
+require fyne.io/fyne/v2 v2.4.3 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+/**
+ * @Author:      leafney
+ * @GitHub:      https://github.com/leafney
+ * @Project:     music-grabber
+ * @Date:        2024-01-13 00:26
+ * @Description:
+ */
+
+package main
+
+func main() {
+
+}
